2e23028fb6255ee1bd01d4b1408f132209e86c48
SunilKumarPradhan <sunilkumarweb47@gmail.com>
2023-07-09 13:40:01 +0530

    can tap the data from the input field but still cant add the data into the list

---
+++ todolist-v1/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day });
+});
+
+app.post('/', (req, res) => {
+  var item = req.body.newItem;
+  console.log(item);
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
